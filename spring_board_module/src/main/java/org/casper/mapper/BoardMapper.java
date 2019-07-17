7366c3445195902f5e8288a42233e228faa1b878
SeyongPark <angryapple05@naver.com>
2019-07-18 00:53:33 +0900

    add Persistence Layer

---
+++ spring_board_module/src/main/java/org/casper/mapper/BoardMapper.java
@@ -0,0 +1,22 @@
+package org.casper.mapper;
+
+import java.util.List;
+
+import org.casper.domain.BoardVO;
+
+public interface BoardMapper {
+
+	//xml 처리로 인한 주석 처리
+	//@Select("select * from tbl_board where bno > 0")
+	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+}
