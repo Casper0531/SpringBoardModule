cd0ac87fd14b2020c80627e08b3df190dae39299
SeyongPark <angryapple05@naver.com>
2019-07-20 00:13:17 +0900

    add Presentation Layer (List, Register, Modify, Delete, Get Page)

--- spring_board_module/src/main/java/org/casper/controller/BoardController.java
+++ spring_board_module/src/main/java/org/casper/controller/BoardController.java
@@ -29,6 +29,11 @@ public class BoardController {
 		model.addAttribute("list", service.getList());
 	}
 	
+	@GetMapping("/register")
+	public void register() {
+		
+	}
+	
 	@PostMapping("/register")
 	public String register(BoardVO board, RedirectAttributes rttr) {
 		
@@ -41,10 +46,10 @@ public class BoardController {
 		return "redirect:/board/list";
 	}
 	
-	@GetMapping("/get")
+	@GetMapping({"/get", "/modify"})
 	public void get(@RequestParam("bno") Long bno, Model model) {
 		
-		log.info("/get");
+		log.info("/get or /modify");
 		
 		model.addAttribute("board", service.get(bno));
 	}
