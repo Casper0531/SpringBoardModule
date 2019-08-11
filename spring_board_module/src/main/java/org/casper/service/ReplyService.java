e1606764011042cc0c9b484b4c9a7f503f2d526d
SeyongPark <angryapple05@naver.com>
2019-08-11 21:25:52 +0900

    Add Foundation for Board's Comment

---
+++ spring_board_module/src/main/java/org/casper/service/ReplyService.java
@@ -0,0 +1,19 @@
+package org.casper.service;
+
+import java.util.List;
+
+import org.casper.domain.Criteria;
+import org.casper.domain.ReplyVO;
+
+public interface ReplyService {
+
+	public int register(ReplyVO vo);
+	
+	public ReplyVO get(Long rno);
+	
+	public int modify(ReplyVO vo);
+	
+	public int remove(Long rno);
+	
+	public List<ReplyVO> getList(Criteria cri, Long bno);
+}
